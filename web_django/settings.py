23f377c9daa91e376d932b7140c4b63b7efac398
lut1k <lut1k@tut.by>
2020-04-20 15:24:10 +0300

    Connected PostgreSQL

--- web_django/settings.py
+++ web_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'web_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
