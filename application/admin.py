9e9bcaba5e844036a91ace7c478104e7da9797f0
lut1k <lut1k@tut.by>
2020-04-21 18:32:40 +0300

    Added models

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from application.models import Profile, Question, Tag, Like, Answer
+
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
+admin.site.register(Like)
--- application/models.py
+++ application/models.py
@@ -1,5 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nick_name = models.CharField(max_length=20, blank=True)
+    avatar = models.ImageField(upload_to=user_directory_path)
+    rating = models.IntegerField(default=0)
+
+
 class Tag(models.Model):
+    name = models.CharField(max_length=31, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    question_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag, blank=True)
+    # TODO почитать что такое 'Answer'
+    correct_answer = models.OneToOneField('Answer', related_name='+', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}; user: {}; updated_at: {}'.format(self.title, self.question_author, self.updated_at)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    answer_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    confirm_flag = models.BooleanField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return "{}, updated_at: {}; text: {}".format(self.answer_author, self.updated_at, self.text)
+
+
+class Like(models.Model):
     pass
