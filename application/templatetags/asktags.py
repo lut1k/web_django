01dacb36a30b0928927594631483284994aab5ac
lut1k <lut1k@tut.by>
2020-06-22 15:06:17 +0300

    Writing top tags and users

---
+++ application/templatetags/asktags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.db.models import Count, Sum, F
+
+from application.models import LaskUser, Tag
+
+register = template.Library()
+
+
+@register.inclusion_tag('include_blocks/best_members.html')
+def top_users(*args):
+    users = LaskUser.objects.all()[:5]
+    return {'users': users}
+
+
+@register.inclusion_tag('include_blocks/popular_tags.html')
+def top_tags(*args):
+    tags = Tag.objects.annotate(count=Count('question')).order_by('-count')[:5]
+    return {'tags': tags}
--- web_django/settings.py
+++ web_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -177,4 +178,10 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-
+# CACHE
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
